2fbaf328255b95566df6495d43f244fc98a69175
brett <brett_matsumoto@yahoo.com>
2019-05-21 18:45:35 -1000

    added getContacts and createUser methods

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -2,10 +2,29 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class BackendService {
-    constructor(private http: HttpClient) {}
+    
+  newUserData: {
+    username: string;
+    name: string;
+    email: string;
+    address: string;
+  } = {
+    username: '',
+    name: '',
+    email: '',
+    address: '',
+  };
 
-    //methods start below
+  constructor(private http: HttpClient) {}
+
+  getContacts(): Promise<object> {
+    return this.http.get('/api/contacts').toPromise();
+  }
+
+  createUser(newUserData): Promise<object> {
+    return this.http.post('/api/register', newUserData).toPromise();
+  }
 }
