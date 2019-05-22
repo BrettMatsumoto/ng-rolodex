7174e02d8503b21595caa7000af84c0b27ca2486
brett <brett_matsumoto@yahoo.com>
2019-05-21 21:25:55 -1000

    removed unused routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
+  // { path: 'login', component: LoginComponent},
+  // { path: 'register', component: RegisterComponent},
   { path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
