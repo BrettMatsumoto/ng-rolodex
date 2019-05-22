0482572f7b71fb3a866e132269517fb67e57d1f5
brett <brett_matsumoto@yahoo.com>
2019-05-21 16:45:53 -1000

    reformatted to only get a particular users profile

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,33 +3,12 @@ const router = express.Router();
 const knex = require('../database/knex');
 const User = require('../database/models/user');
 
-router('/').get((req, res) => {
+router('/', (req, res) => {
   new User()
+    .where({ id: req.params.id })
     .fetchAll()
     .then((results) => {
-      let resultsObj = results.toJSON();
-
-      return res.send(resultsObj);
-    })
-    .put((req, res) => {
-      const body = req.body;
-      new User({ id: body.id })
-        .save({
-          username: body.username,
-          name: body.name,
-          email: body.email,
-          address: body.address,
-        })
-        .then(() => {
-          new Card({ id: body.id })
-            .fetch({ withRelated: ['contacts'] })
-            .then((results) => {
-              return res.json(results);
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-        });
+      return res.send(results.toJSON());
     });
 });
 
