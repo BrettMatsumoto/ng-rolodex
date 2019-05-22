686f3c7cf64f9823c575f4ae69dbf25d3e16e5f2
brett <brett_matsumoto@yahoo.com>
2019-05-21 20:03:58 -1000

    adjusted require for new file name

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../database/knex');
-const User = require('../database/models/user');
+const User = require('../database/models/User');
 const guard = require('../database/middleware/guard');
 
-router.get('/', guard, (req, res) => {
+router.get('/', (req, res) => {
   new User().fetchAll().then((result) => {
-    return res.send(result.toJSON());
+    const resultObj = result.toJSON();
+    return res.send(resultObj);
   });
 });
 
