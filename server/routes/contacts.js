6cf93b4d91a4cc86334f68addd4a2fda917d0449
brett <brett_matsumoto@yahoo.com>
2019-05-21 18:21:59 -1000

    added guard to route and can now ping server, but no response

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -2,5 +2,11 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../database/knex');
 const Contact = require('../database/models/contact');
+const guard = require('../database/middleware/guard');
 
+router.get('/', guard, (req, res) => {
+  new Contact().fetchAll().then((result) => {
+    return res.send(result.toJSON());
+  });
+});
 module.exports = router;
