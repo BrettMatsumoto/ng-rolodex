28b7affa368ec7a3d7daf146b330c07fe8a28cb6
brett <brett_matsumoto@yahoo.com>
2019-05-21 16:16:37 -1000

    added get/put templates to users routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,12 +3,34 @@ const router = express.Router();
 const knex = require('../database/knex');
 const User = require('../database/models/user');
 
-router.get('/', (req, res) => {
-  new User().fetchAll({ withRelated: ['contacts'] }).then((results) => {
-    let resultsObj = results.toJSON();
+router('/').get((req, res) => {
+  new User()
+    .fetchAll()
+    .then((results) => {
+      let resultsObj = results.toJSON();
 
-    return res.send(resultsObj);
-  });
+      return res.send(resultsObj);
+    })
+    .put((req, res) => {
+      const body = req.body;
+      new User({ id: body.id })
+        .save({
+          username: body.username,
+          name: body.name,
+          email: body.email,
+          address: body.address,
+        })
+        .then(() => {
+          new Card({ id: body.id })
+            .fetch({ withRelated: ['contacts'] })
+            .then((results) => {
+              return res.json(results);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        });
+    });
 });
 
 module.exports = router;
