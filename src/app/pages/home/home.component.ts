de4417afc39e3b19ffbb1fcfd94f3735aee9e900
brett <brett_matsumoto@yahoo.com>
2019-05-21 18:44:42 -1000

    removed backend.services code

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../../services/backend.service';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-    users: {
-        username: string,
-        name: string,
-        email: string,
-        address: string,
-    }[];
+  users: {
+    username: string;
+    name: string;
+    email: string;
+    address: string;
+  }[] = [];
 
-    constructor() { }
+  constructor() {}
 
-    ngOnInit() {
-        
-    }
+  ngOnInit() {
+
+  }
 }
