c396d13f84b723dfd1d991d551b354792df75dfe
brett <brett_matsumoto@yahoo.com>
2019-05-21 20:00:43 -1000

    fixed getTable return to users instead of user

---
+++ server/database/models/User.js
@@ -0,0 +1,17 @@
+const bookshelf = require('../bookshelf');
+
+require('./Contact');
+
+class User extends bookshelf.Model {
+    get tableName() {
+        return 'users'
+    }
+    get hasTimestamps() {
+        return true;
+    }
+    contacts() {
+        return this.hasMany('Contact', 'created_by');
+    }
+}
+
+module.exports = bookshelf.model('User', User);
