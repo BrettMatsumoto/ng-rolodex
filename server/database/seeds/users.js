4e0d5972ed143288a8eddfb003a7b6ed9210555b
brett <brett_matsumoto@yahoo.com>
2019-05-21 15:03:56 -1000

    adjusted seeds

--- server/database/seeds/users.js
+++ server/database/seeds/users.js
@@ -2,7 +2,7 @@
 exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
-      return knex('Users').insert([
+      return knex('users').insert([
         {username: 'BeFlowers', name: 'Brenda Flores', email: 'FloralFlowers@email.com', address: '321 Easy Street'},
         {username: 'HIHegs', name: 'Frank Heggeness', email: 'TankMan88@email.com', address: '1155 Buckeye Road'},
         {username: 'ForceWielder30', name: 'Luke Fiorio', email: 'LightSide4Ever@email.com', address: 'T-34 Tatooine Place'},
