01d7cc98c71c2295d4e0684eec37b40f63863597
brett <brett_matsumoto@yahoo.com>
2019-05-21 21:26:53 -1000

    able to display contacts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,22 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService } from '../../services/backend.service';
 
+interface ContactsResponse {
+  name: string,
+  address: string,
+  mobile: string,
+  work: string,
+  home: string,
+  email: string,
+  twitter: string,
+  instagram: string,
+  github: string,
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  users: {
-    username: string;
-    name: string;
-    email: string;
-    address: string;
+  contacts: {
+    name: string,
+    address: string,
+    mobile: string,
+    work: string,
+    home: string,
+    email: string,
+    twitter: string,
+    instagram: string,
+    github: string,
   }[] = [];
 
-  constructor() {}
+  // findUser: {
+  //   name: string;
+  // } = {
+  //   name: '',
+  // };
+
+  // foundUsers: {
+  //   name: string;
+  // }[] = [];
+
+  constructor(private backend: BackendService) {}
 
   ngOnInit() {
-
+    this.backend.getContacts().then((data: ContactsResponse[]) => {
+      this.contacts = data;
+      console.log(data);
+    })
   }
+
+  // submit() {
+  //   console.log(this.findUser);
+  //   const { name } = this.findUser;
+
+  //   this.backend.searchContacts(name).then((data: { name: string }[]) => {
+  //     this.foundUsers = data
+  //   });
+  // }
 }
