7bf99215cd0e22404c17ae00650c3d1821a78e39
brett <brett_matsumoto@yahoo.com>
2019-05-23 09:27:11 -1000

    added methods to header.component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { SessionService } from 'src/app/services/session.service';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss'],
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-    constructor() {}
+  user: {
+    loggedIn: boolean;
+    username: string;
+  };
+  constructor(private auth: AuthService, private router: Router, private session: SessionService) {
+    this.user = this.session.getSession();
+  }
+
+  isLoggedIn() {
+    return this.session.isLoggedIn();
+  }
+
+  login() {
+    return this.router.navigate(['/login']);
+  }
+
+  logout() {
+    return this.auth.logout().then(() => {
+      this.router.navigate(['/login']);
+    });
+  }
 }
