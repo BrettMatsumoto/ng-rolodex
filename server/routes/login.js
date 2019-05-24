8dc4fe3e82c054fb07a09123365345f53b782f11
brett <brett_matsumoto@yahoo.com>
2019-05-24 10:09:38 -1000

    commented out console.logs

--- server/routes/login.js
+++ server/routes/login.js
@@ -5,10 +5,10 @@ const knex = require('../database/knex');
 const Contact = require('../database/models/Contact');
 const guard = require('../database/middleware/guard');
 
-console.log(passport);
+// console.log(passport);
 
 router.post('/', passport.authenticate('local'), (req, res) => {
-  console.log(req);
+  // console.log(req);
   return res.json({
     id: req.user.id,
     username: req.user.username,
