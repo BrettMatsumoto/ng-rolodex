ce9f13742fa7a82e85b707786b10e538ab5e4545
brett <brett_matsumoto@yahoo.com>
2019-05-24 09:21:22 -1000

    added logged in authorization to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService } from '../../services/backend.service';
+import { SessionService } from 'src/app/services/session.service';
+import { Router } from '@angular/router';
 
 interface ContactsResponse {
   name: string,
@@ -31,6 +33,11 @@ export class HomeComponent implements OnInit {
     github: string,
   }[] = [];
 
+  user: {
+    loggedIn: boolean,
+    username: string;
+  }
+
   findUser: {
     name: string;
   } = {
@@ -41,9 +48,19 @@ export class HomeComponent implements OnInit {
     name: string;
   }[] = [];
 
-  constructor(private backend: BackendService) {}
+  constructor(private backend: BackendService, private session: SessionService, private router: Router) {
+    this.user = this.session.getSession();
+  }
+
+  isLoggedIn() {
+    return this.session.isLoggedIn();
+  }
 
   ngOnInit() {
+    if (!this.isLoggedIn()) {
+      this.router.navigate(['/login'])
+    }
+
     this.backend.getContacts().then((data: ContactsResponse[]) => {
       this.contacts = data;
     })
