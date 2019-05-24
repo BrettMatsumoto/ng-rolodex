6b1099ba3fe0648e610ffe8ce9fe691fe239a89f
brett <brett_matsumoto@yahoo.com>
2019-05-24 09:20:30 -1000

    now encrypts password on user registration

--- server/routes/register.js
+++ server/routes/register.js
@@ -3,10 +3,30 @@ const router = express.Router();
 const knex = require('../database/knex');
 const Contact = require('../database/models/Contact');
 const guard = require('../database/middleware/guard');
+const bookshelf = require('bookshelf');
+const bcrypt = require('bcryptjs');
 //require bookshelf
 
-router.post('/api/register', (req, res) => {
-  return res.json({ status: 'ok' });
+const saltRounds = 12
+
+router.post('/', (req, res) => {
+  bcrypt.genSalt(saltRounds, (err, salt) => {
+    if (err) {
+      return 500
+    }
+
+    return new userInfo({
+      username: req.body.username,
+      password: req.body.password,
+      name: req.body.name,
+      email: req.body.email,
+      address: req.body.address,
+    })
+    .save()
+    .then(() => {
+      this.router.navigate(['/']);
+    })
+  })
 });
 
 module.exports = router;
